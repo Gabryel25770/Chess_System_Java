34b5517672d2e65dffd68c1c40a47e9fe43e91ef
GABRYEL25770 <gabryel-lourenco@hotmail.com>
2024-08-16 15:47:40 -0300

    First class: Position

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,10 +1,14 @@
 package entities;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello World");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
